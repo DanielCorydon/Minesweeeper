04a824e424734a840363c26c80500da320767ca5
Daniel Corydon-Petersen <daniel@corydon-petersen.dk>
2021-11-09 15:45:44 +0100

    Created logic in MineField to create all Buttons

--- app/src/main/java/Minesweeeper/Main.java
+++ app/src/main/java/Minesweeeper/Main.java
@@ -3,8 +3,7 @@ package Minesweeeper;
 public class Main {
     public static void main(String args[]){
 
-        MineField f=new MineField();
-        f.setSize(350,350);//frame size 350 width and 350 height
+        MineField f=new MineField(35, 35);
         f.setLayout(null);//no layout now bydefault BorderLayout
         f.setVisible(true);//now frame willbe visible, bydefault not visible
     }
--- app/src/main/java/Minesweeeper/MineField.java
+++ app/src/main/java/Minesweeeper/MineField.java
@@ -1,30 +1,48 @@
 package Minesweeeper;
 import java.awt.*;
+import java.util.ArrayList;
 
 class MineField extends Frame{
 
-    MineField(){
+    MineField(int width, int height){
+        this.setSize(height*10,width*10);//frame size 350 width and 350 height
         
-        int positionx = 0;
-        int positiony = 0;
-        int counter = 0;
-        for (int i = 0; i < 10; i++) {
-            positiony = 0;
-            positionx += 30;
-            for (int j = 0; j < 10; j++) {
-                counter++;
-                positiony += 30;
-                boolean isBomb = (counter % 2 == 0) ? true : false;
-                String bombString = (counter % 2 == 0) ? "B" : "X";
-                Button b=new MineButton(bombString, isBomb);
-                b.setBounds(positionx,positiony,30,30);// setting button position
+    }
+    public void initializeField(int bombAmount) {
+        
+    }
+
+    // Creates ArrayList of [amount] Integers from 0 to [limit]
+    private ArrayList<Integer> getRandomList(int amount) {
+        ArrayList<Integer> numList = new ArrayList<>();
+            while (numList.size() < amount) {
+                int randomNum = Math.round( (long) Math.random() * (this.getHeight() * this.getWidth()));
+                if (!numList.contains(randomNum)) {
+                    numList.add(randomNum);
+                }
+            }
+        return numList;
+    }
+    // Creates an ArrayList of ArrayLists containg boolean values based on the parameter bombLocations
+    private ArrayList<ArrayList<Boolean>> createButtonInfo(ArrayList<Integer> bombLocations) {
+        ArrayList<ArrayList<Boolean>> buttonList = new ArrayList<ArrayList<Boolean>>();
+        for (int i = 0; i < this.getWidth(); i++) {
+            for (int j = 0; j < this.getHeight(); j++) {
+                boolean isBomb = (bombLocations.contains(i * 10 + j)) ? true : false;
+                buttonList.get(i).add(j, isBomb);
+            }
+        }
+        return buttonList;
+    }
+
+    private void createButtons(ArrayList<ArrayList<Boolean>> buttonList) {
+        for (int i = 0; i < buttonList.size(); i++) {
+            for (int j = 0; j < buttonList.get(i).size(); j++) {
+                ArrayList<MineButton> nearbyButtons = new ArrayList<MineButton>();
+                Button b=new MineButton(buttonList.get(i).get(j));
+                b.setBounds(i * 30,j * 30,30,30);// setting button position
                 add(b);//adding button into frame
             }
         }
-        
-        setSize(350,350);//frame size 350 width and 350 height
-        setLayout(null);//no layout now bydefault BorderLayout
-        setVisible(true);//now frame willbe visible, bydefault not visible
-
     }
 }
