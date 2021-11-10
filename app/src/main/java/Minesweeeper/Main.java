85220b5f7a8ae0feee9d4261996f4f2ba5bdcad2
Nicholas Gioachini <ngio@itu.dk>
2021-11-10 09:22:38 +0100

    Tested and formatted files

--- app/src/main/java/Minesweeeper/Main.java
+++ app/src/main/java/Minesweeeper/Main.java
@@ -1,11 +1,11 @@
 package Minesweeeper;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        MineField mineField=new MineField(15, 15);
+        MineField mineField = new MineField(10, 10);
         mineField.initializeField(30);
-        mineField.setLayout(null);//no layout now bydefault BorderLayout
-        mineField.setVisible(true);//now frame willbe visible, bydefault not visible
+        mineField.setLayout(null);// no layout now bydefault BorderLayout
+        mineField.setVisible(true);// now frame willbe visible, bydefault not visible
     }
 }
--- app/src/main/java/Minesweeeper/MineField.java
+++ app/src/main/java/Minesweeeper/MineField.java
@@ -1,18 +1,21 @@
 package Minesweeeper;
+
 import java.awt.*;
 import java.util.ArrayList;
 
 import org.checkerframework.checker.units.qual.min;
 
-class MineField extends Frame{
+class MineField extends Frame {
     private int buttonsX;
     private int buttonsY;
-    MineField(int width, int height){
-        this.setSize(height*30 + 60,width*30 + 60);//frame size set by arguments
+
+    MineField(int width, int height) {
+        this.setSize(height * 30 + 60, width * 30 + 60);// frame size set by arguments
         this.buttonsX = width;
-        this.buttonsY = height; 
-        
+        this.buttonsY = height;
+
     }
+
     public void initializeField(int bombAmount) {
         ArrayList<Integer> numList = getRandomList(bombAmount);
         ArrayList<ArrayList<MineButton>> bombList = createButtons(numList);
@@ -23,15 +26,17 @@ class MineField extends Frame{
     private ArrayList<Integer> getRandomList(int amount) {
         System.out.println("getrandomworks");
         ArrayList<Integer> numList = new ArrayList<>();
-            while (numList.size() < amount) {
-                long randomNum = Math.round(Math.random() * (buttonsX * buttonsY));
-                if (!numList.contains(randomNum)) {
-                    numList.add((int)randomNum);
-                }
+        while (numList.size() < amount) {
+            long randomNum = Math.round(Math.random() * (buttonsX * buttonsY));
+            if (!numList.contains(randomNum)) {
+                numList.add((int) randomNum);
             }
+        }
         return numList;
     }
-    // Creates an ArrayList of ArrayLists containg boolean values based on the parameter bombLocations
+
+    // Creates an ArrayList of ArrayLists containg boolean values based on the
+    // parameter bombLocations
     private ArrayList<ArrayList<MineButton>> createButtons(ArrayList<Integer> bombLocations) {
         System.out.println("createbuttons works");
         ArrayList<ArrayList<MineButton>> buttonList = new ArrayList<ArrayList<MineButton>>();
@@ -42,7 +47,7 @@ class MineField extends Frame{
                 MineButton mineButton = new MineButton(isBomb);
                 mineButton.addActionListener(mineButton.getThisAction());
                 buttonList.get(i).add(mineButton);
-                mineButton.setBounds((i + 1) *30,(j + 1)*30,30,30);    
+                mineButton.setBounds((i + 1) * 30, (j + 1) * 30, 30, 30);
                 this.add(mineButton);
             }
         }
@@ -57,15 +62,16 @@ class MineField extends Frame{
                 int counter = 0;
                 for (int k = -1; k < 2; k++) {
                     for (int k2 = -1; k2 < 2; k2++) {
-                        if (!(k == 0 && k2 == 0) && i + k >= 0 && j + k2 >= 0 && i + k < buttonList.size() && j + k2 < buttonList.get(i).size() ) {
-                            nearbyButtons.add(buttonList.get(i+k).get(j+k2));
-                            if (buttonList.get(i+k).get(j+k2).getIsBomb()) {
+                        if (!(k == 0 && k2 == 0) && i + k >= 0 && j + k2 >= 0 && i + k < buttonList.size()
+                                && j + k2 < buttonList.get(i).size()) {
+                            nearbyButtons.add(buttonList.get(i + k).get(j + k2));
+                            if (buttonList.get(i + k).get(j + k2).getIsBomb()) {
                                 counter++;
                             }
-                        }      
+                        }
                     }
                 }
-                buttonList.get(i).get(j).getNeighbors(nearbyButtons,counter);
+                buttonList.get(i).get(j).getNeighbors(nearbyButtons, counter);
             }
         }
     }
